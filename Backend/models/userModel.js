db0d66d0599762f379378683fdc4e1fbaa6ff5cb
Saif <swaifweaa@gmail.com>
2023-10-03 11:06:16 +0300

    Saif - Small Improvements - Fixes #1

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -3,34 +3,35 @@ import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv/config';
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {//to not change password every time we edit the user data
-    next();
-  }
-  const salt = await bcrypt.genSalt(+process.env.SALT);
-  this.password = await bcrypt.hash(this.password, salt);
+    if (!this.isModified('password')) {
+        //to not change password every time we edit the user data
+        next();
+    }
+    const salt = await bcrypt.genSalt(+process.env.SALT);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 userSchema.methods.comparePassword = async function (enteredPassword) {
-  const isMatch = await bcrypt.compare(enteredPassword, this.password)
-  return isMatch
-}
+    const isMatch = await bcrypt.compare(enteredPassword, this.password);
+    return isMatch;
+};
 
 const User = mongoose.model('Users', userSchema);
 export { User };
