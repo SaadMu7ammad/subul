953ab66c69ec09ed9fe14ecbbe932de781d6ccb1
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-10-08 22:15:22 +0300

    fix error after merge

--- Src/Routes/charityRoutes.js
+++ Src/Routes/charityRoutes.js
@@ -1,16 +1,18 @@
 import express, { Router } from 'express';
 import { auth } from '../middlewares/authMiddleware.js';
-import { registerCharity,authCharity,activateCharityAccount ,requestResetPassword} from '../Controllers/charityController.js';
 import {
   registerCharity,
   uploadCoverImage,
+  authCharity,
+  activateCharityAccount,
+  requestResetPassword,
 } from '../Controllers/charityController.js';
 
 const router = express.Router();
 
 router.post('/', uploadCoverImage, registerCharity);
-router.post('/auth',authCharity);
-router.post('/activate',auth,activateCharityAccount);
-router.post('/reset',auth,requestResetPassword);
+router.post('/auth', authCharity);
+router.post('/activate', auth, activateCharityAccount);
+router.post('/reset', auth, requestResetPassword);
 
 export default router;
