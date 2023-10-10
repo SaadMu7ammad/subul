15b43c3250a26073d6d11ca39f1b36a05a71afc7
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-10-10 11:50:35 +0300

    seperate the imgStuff to a new middleware

--- Src/Controllers/charityController.js
+++ Src/Controllers/charityController.js
@@ -1,14 +1,9 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { v4 as uuidv4 } from 'uuid';
 
 import Charity from '../models/charityModel.js';
 import generateToken from '../utils/generateToken.js';
 import asyncHandler from 'express-async-handler';
 import { setupMailSender, generateResetTokenTemp } from '../utils/mailer.js';
-import multer from 'multer';
-import sharp from 'sharp';
+
 import {
   BadRequestError,
   CustomAPIError,
@@ -16,50 +11,6 @@ import {
   UnauthenticatedError,
 } from '../errors/index.js';
 import logger from '../utils/logger.js';
-//disk Storage solution
-// const multerStorage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     // cb(null, 'uploads/');
-//     cb(null, './uploads/LogoCharities');
-//   },
-//   filename: function (req, file, cb) {
-//     // const imageUrl = file.path.replace("\\", "/");
-//     const ex = file.mimetype.split('/')[1];
-//     const uniqueSuffix ="LogoCharity"+uuidv4()+"-"+ Date.now() ;
-//     cb(null, uniqueSuffix + '.' + ex);
-//   },
-// });
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image')) {
-    //accepts imgs only
-    cb(null, true);
-  } else {
-    cb(new BadRequestError('invalid type,Only images allowed'));
-  }
-};
-const resizeImg = asyncHandler(async (req, res, next) => {
-  // const ex = file.mimetype.split('/')[1];
-  const uniqueSuffix = 'LogoCharity' + uuidv4() + '-' + Date.now();
-  const filename = uniqueSuffix + '.jpeg';
-  await sharp(req.file.buffer)
-    .resize(320, 240)
-    .toFormat('jpeg')
-    .jpeg({ quality: 90 })
-    .toFile('./uploads/LogoCharities/' + filename); //, (err, info) => {
-  //   console.log('err');
-  //   console.log(err);
-  // });
-  //adding the filename in the req.body
-  req.body.image = filename;
-  next();
-});
-//diskStorage
-// const upload = multer({ storage: multerStorage,fileFilter:multerFilter });
-// const uploadCoverImage = upload.single('image');
-//memoryStorage
-const multerStorage = multer.memoryStorage();
-const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
-const uploadCoverImage = upload.single('image');
 
 const registerCharity = asyncHandler(async (req, res, next) => {
   // logger.info(req.file.path);
@@ -181,6 +132,5 @@ export {
   authCharity,
   activateCharityAccount,
   requestResetPassword,
-  uploadCoverImage,
-  resizeImg,
+
 };
--- Src/Routes/charityRoutes.js
+++ Src/Routes/charityRoutes.js
@@ -1,30 +1,18 @@
 import express, { Router } from 'express';
-import multer from 'multer';
 
 import { auth } from '../middlewares/authMiddleware.js';
+
 import {
   registerCharity,
-  uploadCoverImage,
   authCharity,
   activateCharityAccount,
   requestResetPassword,
-  resizeImg,
 } from '../Controllers/charityController.js';
+import { resizeImg, uploadCoverImage } from '../middlewares/imageMiddleware.js';
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
 
-router.post(
-  '/',
-  // upload.single('image'),
-  // (req, res, next) => {
-  //   req.file.path=req.file.path.replace("\\" ,"/")
-  //   console.log(req.file);
-  //   next();
-  // },
-  uploadCoverImage,resizeImg,
-  registerCharity
-);
+router.post('/', uploadCoverImage, resizeImg, registerCharity);
 router.post('/auth', authCharity);
 router.post('/activate', auth, activateCharityAccount);
 router.post('/reset', auth, requestResetPassword);
