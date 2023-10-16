0b2b26ead8c8ead6a4cf2c76db74db9ddfa3b91f
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-10-16 15:28:15 +0300

    add and get cases #51

--- Src/Controllers/casesController.js
+++ Src/Controllers/casesController.js
@@ -5,9 +5,24 @@ import { BadRequestError } from '../errors/bad-request.js';
 import { setupMailSender } from '../utils/mailer.js';
 
 const addCase = asyncHandler(async (req, res, next) => {
-  const cases = await Cases.create(req.body);
-  req.charity.cases.push(cases._id);
+  const newCase = await Cases.create(req.body);
+  req.charity.cases.push(newCase._id);
   await req.charity.save();
   res.json(req.charity);
 });
-export { addCase };
+const getAllCases = asyncHandler(async (req, res, next) => {
+  const _case = await req.charity.populate({
+    path: 'cases',
+    model: 'Cases',
+    select: '-_id',
+  }); //.exec();
+  res.json(_case);
+});
+
+const getCaseById = asyncHandler(async (req, res, next) => {
+  const _case = await Cases.findById(req.params.id);
+  if (!_case) throw new BadRequestError('id not found');
+
+  res.json(_case);
+});
+export { addCase, getAllCases, getCaseById };
