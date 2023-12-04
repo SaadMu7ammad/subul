9f0d4636352c6cde140fae7b04ef6f8e0535adcb
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-12-04 19:50:31 +0200

    cont creating transaction

--- Src/services/transaction.service.js
+++ Src/services/transaction.service.js
@@ -1,24 +1,70 @@
 import asyncHandler from 'express-async-handler';
-import Cases from '../models/caseModel.js';
+import Case from '../models/caseModel.js';
 import Transactions from '../models/transactionModel.js';
 import { NotFoundError } from '../errors';
 const createTransaction = asyncHandler(async (data) => {
-  const { caseId, donationAmount } = data;
-  if (donationAmount === 0) {
+  const { caseId, moneyPaid, paymentMethod } = data;
+  //pre processing
+  if (!caseId) {
+    throw new NotFoundError('id is not found');
+  }
+  if (moneyPaid === 0) {
     throw new BadRequestError('Invalid amount of donation');
   }
-  const caseIsExist = await Cases.findById(caseId);
+  if (!paymentMethod) {
+    throw new NotFoundError('no payment method has been chosen');
+  }
+  if (
+    !paymentMethod.bankAccount &&
+    !paymentMethod.fawry &&
+    !paymentMethod.vodafoneCash
+  ) {
+    throw new NotFoundError('no payment method has been chosen');
+  }
+//   let paymentType;
+//   if (paymentMethod.bankAccount) paymentType = 'Bank';
+//   else if (paymentMethod.fawry) paymentType = 'fawry';
+//   else if (paymentMethod.vodafoneCash) paymentType = 'vodafoneCash';
+
+  const caseIsExist = await Case.findById(caseId);
   if (!caseIsExist) {
-    throw new NotFoundError('case is not founded');
+    throw new NotFoundError('case is not found');
   }
   //check the case is finished or being freezed by the website admin
   if (caseIsExist.finished === true || caseIsExist.freezed === true) {
     throw new BadRequestError(
-      'this case is finished...choose not completed one'
+      'this case is finished...choose case not completed'
     );
   }
-    const transaction = await Transactions.create
-    ({})
+
+  const transaction = await Transactions.create({
+    case: caseId,
+    user: req.user._id,
+    moneyPaid: moneyPaid,
+    paymentMethod: paymentMethod
+  });
+  if (!transaction) throw new BadRequestError('no transaction found');
+  //after transaction created we must update the case info
+
+  //first..check that donor only donates with the remain part of money and not exceed the target amount
+  const currentAmount = caseIsExist.currentDonationAmount + moneyPaid;
+
+  if (caseIsExist.targetDonationAmount < currentAmount) {
+    throw new BadRequestError(
+      'please donate with the remained value which is ' +
+        (caseIsExist.targetDonationAmount - caseIsExist.currentDonationAmount)
+    );
+  }
+  //second..check if that is the last donation to finish this case
+  if (caseIsExist.targetDonationAmount === currentAmount) {
+    caseIsExist.dateFinished = Date.now();
+    caseIsExist.finished = true;
+  }
+  //update the case info
+  caseIsExist.dontationNumbers++;
+  caseIsExist.currentDonationAmount += moneyPaid;
+  await caseIsExist.save();
+
   return data;
 });
 
