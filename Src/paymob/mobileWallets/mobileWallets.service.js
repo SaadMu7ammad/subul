1717990fddc6815d28f8d6bc6b844d50fc4aab8f
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-12-09 21:29:32 +0200

    using fetch and remove axios

--- Src/paymob/mobileWallets/mobileWallets.service.js
+++ Src/paymob/mobileWallets/mobileWallets.service.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import dotenv from 'dotenv/config';
 import asyncHandler from 'express-async-handler';
 
@@ -8,49 +7,55 @@ let amount = 500;
 let merch;
 let tokenThirdStep;
 const CreateAuthenticationRequestMobileWallet = async (req, res, next) => {
-  const response = await axios.post(
-    'https://accept.paymob.com/api/auth/tokens',
-    {
-      api_key: process.env.PAYMOB_API_KEY,
-    }
-  );
-  const { data } = response;
-
+  const request = await fetch('https://accept.paymob.com/api/auth/tokens', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ api_key: process.env.PAYMOB_API_KEY }),
+  });
+  const response = await request.json();
   // Extract relevant data from the response
-  token = data.token;
+  token = response.token;
 
   console.log(token);
   console.log(orderId);
   console.log(amount);
+  console.log(tokenThirdStep);
 
-  return res.status(201).json({ data: response.data });
+  return res.status(201).json({ data: response });
 };
 
 const OrderRegistrationAPIMobileWallet = async (req, res) => {
-  let response = await axios.post(
+  let request = await fetch(
     `https://accept.paymobsolutions.com/api/ecommerce/orders`,
     {
-      auth_token: token,
-      delivery_needed: false,
-      amount_cents: amount * 100, //.toString(),
-      currency: 'EGP',
-      // merchant_order_id: Math.floor(Math.random() * 10),
-      items: [
-        {
-          name: 'case',
-          amount_cents: '500000',
-          description: 'poor man',
-          quantity: '1',
-        },
-      ],
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        auth_token: token,
+        delivery_needed: false,
+        amount_cents: amount * 100, //.toString(),
+        currency: 'EGP',
+        // merchant_order_id: Math.floor(Math.random() * 10),
+        items: [
+          {
+            name: 'case',
+            amount_cents: '500000',
+            description: 'poor man',
+            quantity: '1',
+          },
+        ],
+      }),
     }
   );
-  orderId = response.data.id;
+  const response = await request.json();
+  orderId = response.id;
   console.log(token);
   console.log(orderId);
   console.log(amount);
+  console.log(tokenThirdStep);
+
   //   console.log(merchant_order_id);
-  return res.status(201).json({ data: response.data });
+  return res.status(201).json({ data: response });
 };
 const PaymentKeyRequestMobileWallet = async (req, res) => {
   try {
@@ -59,38 +64,46 @@ const PaymentKeyRequestMobileWallet = async (req, res) => {
       lastName: 'Momo',
       phone: '01021533501',
     };
-    let response = await axios.post(
+    let request = await fetch(
       `https://accept.paymob.com/api/acceptance/payment_keys`,
       {
-        auth_token: token,
-        amount_cents: amount * 100,
-        expiration: 3600,
-        orderId,
-        billing_data: {
-          email: user.email || 'NA',
-          phone_number: user.phone,
-          apartment: user.address ? user.address.apartment : 'NA',
-          floor: user.address ? user.address.floor : 'NA',
-          building: user.address ? user.address.building : 'NA',
-          street: user.address ? user.address.street : 'NA',
-          city: user.address ? user.address.city : 'NA',
-          country: user.address ? user.address.country : 'NA',
-          first_name: user.firstName,
-          last_name: user.lastName,
-          state: user.address ? user.address.state : 'NA',
-          zip_code: user.address ? user.address.zip_code : 'NA',
-        },
-        currency: 'EGP',
-        integration_id: +process.env.PAYMOB_MOBILE_WALLET_INTEGRATION_ID,
-        lock_order_when_paid: 'false',
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          auth_token: token,
+          amount_cents: amount * 100,
+          expiration: 3600,
+          orderId,
+          billing_data: {
+            email: user.email || 'NA',
+            phone_number: user.phone,
+            apartment: user.address ? user.address.apartment : 'NA',
+            floor: user.address ? user.address.floor : 'NA',
+            building: user.address ? user.address.building : 'NA',
+            street: user.address ? user.address.street : 'NA',
+            city: user.address ? user.address.city : 'NA',
+            country: user.address ? user.address.country : 'NA',
+            first_name: user.firstName,
+            last_name: user.lastName,
+            state: user.address ? user.address.state : 'NA',
+            zip_code: user.address ? user.address.zip_code : 'NA',
+          },
+          currency: 'EGP',
+          integration_id: +process.env.PAYMOB_MOBILE_WALLET_INTEGRATION_ID,
+          lock_order_when_paid: 'false',
+        }),
       }
     );
+    const response = await request.json();
+
     //   return response.data.token;
+    console.log(token);
+    tokenThirdStep = response.token;
     console.log(token);
     console.log(orderId);
     console.log(amount);
-    tokenThirdStep = response.data.token;
-    return res.status(201).json({ data: response.data });
+    console.log(tokenThirdStep);
+    return res.status(201).json({ data: response });
     // return tokenThirdStep;
   } catch (error) {
     throw new Error(error);
@@ -99,17 +112,22 @@ const PaymentKeyRequestMobileWallet = async (req, res) => {
 
 //after three configuration stages done
 const paywithMobileWallet = asyncHandler(async (req, res, next) => {
-  let response = await axios.post(
+  let request = await fetch(
     ` https://accept.paymob.com/api/acceptance/payments/pay`,
     {
-      payment_token: tokenThirdStep,
-      source: {
-        identifier: '01010101010',
-        subtype: 'WALLET',
-      },
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        payment_token: tokenThirdStep,
+        source: {
+          identifier: '01010101010',
+          subtype: 'WALLET',
+        },
+      }),
     }
   );
-  return res.status(201).json({ data: response.data });
+  const response = await request.json();
+  return res.status(201).json({ data: response });
 });
 
 export {
--- Src/paymob/onlineCards/onlineCards.service.js
+++ Src/paymob/onlineCards/onlineCards.service.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
 import dotenv from 'dotenv/config';
-import asyncHandler from 'express-async-handler';
 
 let token;
 let orderId;
@@ -8,104 +6,113 @@ let amount = 2000;
 let merch;
 let tokenThirdStep;
 const CreateAuthenticationRequestOnlineCard = async (req, res, next) => {
-  const response = await axios.post(
-    'https://accept.paymob.com/api/auth/tokens',
-    {
-      api_key: process.env.PAYMOB_API_KEY,
-    }
-  );
-  const { data } = response;
+  const request = await fetch('https://accept.paymob.com/api/auth/tokens', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ api_key: process.env.PAYMOB_API_KEY }),
+  });
+  const response = await request.json();
 
   // Extract relevant data from the response
-  token = data.token;
+  token = response.token;
 
   console.log(token);
   console.log(orderId);
   console.log(amount);
   console.log(tokenThirdStep);
 
-  return res.status(201).json({ data: response.data });
+  return res.status(201).json({ data: response });
 };
 
 const OrderRegistrationAPIOnlineCard = async (req, res) => {
-  let response = await axios.post(
+  let request = await fetch(
     `https://accept.paymobsolutions.com/api/ecommerce/orders`,
     {
-      auth_token: token,
-      delivery_needed: false,
-      amount_cents: amount * 100, //.toString(),
-      currency: 'EGP',
-      // merchant_order_id: Math.floor(Math.random() * 10),
-      items: [
-        {
-          name: 'case',
-          amount_cents: '500000',
-          description: 'poor man',
-          quantity: '1',
-        },
-      ],
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        auth_token: token,
+        delivery_needed: false,
+        amount_cents: amount * 100, //.toString(),
+        currency: 'EGP',
+        // merchant_order_id: Math.floor(Math.random() * 10),
+        items: [
+          {
+            name: 'case',
+            amount_cents: '500000',
+            description: 'poor man',
+            quantity: '1',
+          },
+        ],
+      }),
     }
   );
-  orderId = response.data.id;
+  const response = await request.json();
+  orderId = response.id;
   console.log(token);
   console.log(orderId);
   console.log(amount);
   console.log(tokenThirdStep);
 
   //   console.log(merchant_order_id);
-  return res.status(201).json({ data: response.data });
+  return res.status(201).json({ data: response });
 };
-const PaymentKeyRequestOnlineCard = asyncHandler(async (req, res) => {
+const PaymentKeyRequestOnlineCard = async (req, res) => {
   try {
     const user = {
       firstName: 'Saul',
       lastName: 'Momo',
       phone: '01021533501',
     };
-    let response = await axios.post(
+    let request = await fetch(
       `https://accept.paymob.com/api/acceptance/payment_keys`,
       {
-        auth_token: token,
-        amount_cents: amount * 100,
-        expiration: 3600,
-        orderId,
-        billing_data: {
-          email: user.email || 'NA',
-          phone_number: user.phone,
-          apartment: user.address ? user.address.apartment : 'NA',
-          floor: user.address ? user.address.floor : 'NA',
-          building: user.address ? user.address.building : 'NA',
-          street: user.address ? user.address.street : 'NA',
-          city: user.address ? user.address.city : 'NA',
-          country: user.address ? user.address.country : 'NA',
-          first_name: user.firstName,
-          last_name: user.lastName,
-          state: user.address ? user.address.state : 'NA',
-          zip_code: user.address ? user.address.zip_code : 'NA',
-        },
-        currency: 'EGP',
-        integration_id: +process.env.PAYMOB_CREDIT_CARD_INTEGRATION_ID,
-        lock_order_when_paid: 'false',
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          auth_token: token,
+          amount_cents: amount * 100,
+          expiration: 3600,
+          orderId,
+          billing_data: {
+            email: user.email || 'NA',
+            phone_number: user.phone,
+            apartment: user.address ? user.address.apartment : 'NA',
+            floor: user.address ? user.address.floor : 'NA',
+            building: user.address ? user.address.building : 'NA',
+            street: user.address ? user.address.street : 'NA',
+            city: user.address ? user.address.city : 'NA',
+            country: user.address ? user.address.country : 'NA',
+            first_name: user.firstName,
+            last_name: user.lastName,
+            state: user.address ? user.address.state : 'NA',
+            zip_code: user.address ? user.address.zip_code : 'NA',
+          },
+          currency: 'EGP',
+          integration_id: +process.env.PAYMOB_CREDIT_CARD_INTEGRATION_ID,
+          lock_order_when_paid: 'false',
+        }),
       }
     );
+    const response = await request.json();
+
     //   return response.data.token;
-    tokenThirdStep = response.data.token;
+    console.log(token);
+    tokenThirdStep = response.token;
     console.log(token);
     console.log(orderId);
     console.log(amount);
-    // console.log(response);
     console.log(tokenThirdStep);
-    console.log('-------------');
-    return res.status(201).json({ data: response.data });
+    return res.status(201).json({ data: response });
     // return tokenThirdStep;
   } catch (error) {
     throw new Error(error);
   }
-});
+};
 
 //after three configuration stages done
 //we have 2 choices
-const payWithOnlineCard = async (req, res, next) =>{
+const payWithOnlineCard = async (req, res, next) => {
   console.log(tokenThirdStep);
   const CardPayments = `https://accept.paymobsolutions.com/api/acceptance/iframes/${process.env.PAYMOB_FRAME_ID}?payment_token=${tokenThirdStep}`;
   return res.status(201).json({ CardPayments });
