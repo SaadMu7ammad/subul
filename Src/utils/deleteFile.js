726102bf727d3626e4dd9e0dc78bb9f24c34f6f8
SaifLotfi <swaiflnkac@gmail.com>
2024-01-01 19:46:19 +0200

    adding general delete img funciton

--- Src/utils/deleteFile.js
+++ Src/utils/deleteFile.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import logger from './logger.js';
+import 'dotenv/config.js'
 const deleteFile = (filePath) => {
     logger.warn(filePath);
     fs.unlink(filePath, (err) => {
@@ -9,17 +10,34 @@ const deleteFile = (filePath) => {
     });
 };
 
-const deleteOldImgs = (arr, selector) => {
-    arr.map((img) => {
-        const oldImagePath = path.join('./uploads/docsCharities', img);
-        if (fs.existsSync(oldImagePath)) {
+const deleteFiles = (pathToFolder, folderName, ...filesNames) => {
+    filesNames.forEach((fileName) => {
+        const filePath = path.join(pathToFolder, folderName, fileName);
+        if (fs.existsSync(filePath)) {
             // Delete the file
-            fs.unlinkSync(oldImagePath);
-            console.log('Old image deleted successfully.');
+            fs.unlinkSync(filePath);
+            console.log('File deleted successfully.');
         } else {
-            console.log('Old image does not exist.');
+            console.log('File does not exist.');
         }
     });
-    arr = [];
 };
-export { deleteFile ,deleteOldImgs};
+
+const deleteOldImgs = (imgsFolder, imgsNames) => {
+    const imgsNamesArray = Array.isArray(imgsNames) ? imgsNames : [imgsNames];
+
+    if (process.env.NODE_ENV === 'development') {
+        deleteFiles('./uploads', imgsFolder, ...imgsNamesArray);
+    } else if (process.env.NODE_ENV === 'production') {
+        imgsNamesArray.forEach((imgName) => {
+            deleteImgFromCloudinary(imgsFolder, imgName.split('.jpeg')[0]);
+        });
+    }
+
+    imgsNamesArray.length = 0;
+};
+
+deleteOldImgs('casesCoverImages', 'wallpaperflare.com_wallpaper.jpg');
+
+
+export { deleteFile , deleteFiles , deleteOldImgs };
