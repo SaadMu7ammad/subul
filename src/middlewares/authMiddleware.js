948a6e3ee82841fef7f5cb160755fa1b973aecdd
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2024-01-25 23:45:20 +0200

    check the db in middleware for less conditions in code

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,33 +2,34 @@ import jwt from 'jsonwebtoken';
 
 import asyncHandler from 'express-async-handler';
 
-import  User from '../models/userModel.js';
-import Charity  from '../models/charityModel.js';
+import User from '../models/userModel.js';
+import Charity from '../models/charityModel.js';
 import { UnauthenticatedError } from '../errors/unauthenticated.js';
 import logger from '../utils/logger.js';
 
 const auth = asyncHandler(async (req, res, next) => {
   const authCookie = req.cookies.jwt; //according to cookie parser
-
   if (!authCookie) {
     throw new UnauthenticatedError('Authentication invalid');
   }
-  try {
-    const decoded = jwt.verify(authCookie, process.env.JWT_SECRET);
-    // attach the user to the job routes
-    if(decoded.userId){
-      req.user = await User.findById(decoded.userId).select('-password');
-    }else if(decoded.charityId){
+  const decoded = jwt.verify(authCookie, process.env.JWT_SECRET);
+  // attach the user to the job routes
+  if (decoded.userId) {
+    // check first the user or chariy exists in the db
+    const IsUserExist = await User.findById(decoded.userId).select('-password');
+    if (!IsUserExist) throw new UnauthenticatedError('Authentication invalid');
+    req.user = IsUserExist;
+  } else if (decoded.charityId) {
+    const IsCharityExist = await Charity.findById(decoded.charityId).select(
+      '-password'
+    );
+    if (!IsCharityExist)
+      throw new UnauthenticatedError('Authentication invalid');
 
-      req.charity = await Charity.findById(decoded.charityId).select('-password');
-    }
-    // console.log(req.user);
-    next();
-  } catch (error) {
-    throw new UnauthenticatedError('Authentication invalid');
+    req.charity = IsCharityExist;
   }
+  // console.log(req.user);
+  next();
 });
-
-
 
 export { auth };
