aa977592c254f32999a0659608e7c29e3e1343aa
SaifLotfi <swaiflnkac@gmail.com>
2024-01-30 11:03:00 +0200

    added defineRoutes function to user routers and moved them to api folder - configuration provider and entry point server are not complete

---
+++ src/components/auth/user/entry-points/api/auth.routes.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import logger from '../../../../../utils/logger.js';
+import { authUseCase } from '../../domain/auth.use-case.js';
+import {
+    loginUserValidation,
+    registerUserValidation,
+} from '../../../../../libraries/validation/components/user/userAuthValidation.js';
+import { validate } from '../../../../../libraries/validation/index.js';
+
+export default function defineRoutes(expressApp) {
+    const router = express.Router();
+
+    router.post(
+        '/',
+        registerUserValidation,
+        validate,
+        async (req, res, next) => {
+            try {
+                logger.info(`Auth API was called to register User`);
+                const authResponse = await authUseCase.registerUser(
+                    req,
+                    res,
+                    next
+                );
+                return res.json(authResponse);
+            } catch (error) {
+                next(error);
+                return undefined;
+            }
+        }
+    );
+
+    router.post(
+        '/auth',
+        loginUserValidation,
+        validate,
+        async (req, res, next) => {
+            try {
+                logger.info(`Auth API was called to auth User`);
+                const authResponse = await authUseCase.authUser(req, res, next);
+                return res.json(authResponse);
+            } catch (error) {
+                next(error);
+                return undefined;
+            }
+        }
+    );
+    expressApp.use('/api/users', router);
+}
