c01bb096d1a36ae3a912b038fff255497d701561
SaifLotfi <swaiflnkac@gmail.com>
2024-02-21 21:24:40 +0200

    added charity types to charity.model #148

--- src/components/charity/data-access/models/charity.model.ts
+++ src/components/charity/data-access/models/charity.model.ts
@@ -1,114 +1,119 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import * as configurationProvider from '../../../../libraries/configuration-provider/index.js';
+import {
+    CharitySchema,
+    CharityModel,
+    CharityDocument,
+} from '../interfaces/charity.interface.js';
 const Schema = mongoose.Schema;
 
 const locationSchema = new mongoose.Schema({
-  governorate: {
-    type: String,
-    enum: [
-      'Alexandria',
-      'Assiut',
-      'Aswan',
-      'Beheira',
-      'Bani Suef',
-      'Cairo',
-      'Daqahliya',
-      'Damietta',
-      'Fayyoum',
-      'Gharbiya',
-      'Giza',
-      'Helwan',
-      'Ismailia',
-      'Kafr El Sheikh',
-      'Luxor',
-      'Marsa Matrouh',
-      'Minya',
-      'Monofiya',
-      'New Valley',
-      'North Sinai',
-      'Port Said',
-      'Qalioubiya',
-      'Qena',
-      'Red Sea',
-      'Sharqiya',
-      'Sohag',
-      'South Sinai',
-      'Suez',
-      'Tanta',
-    ],
-    required: true,
-  },
-  city: {
-    type: String,
-    required: false,
-  },
-  street: {
-    type: String,
-    required: false,
-  },
+    governorate: {
+        type: String,
+        enum: [
+            'Alexandria',
+            'Assiut',
+            'Aswan',
+            'Beheira',
+            'Bani Suef',
+            'Cairo',
+            'Daqahliya',
+            'Damietta',
+            'Fayyoum',
+            'Gharbiya',
+            'Giza',
+            'Helwan',
+            'Ismailia',
+            'Kafr El Sheikh',
+            'Luxor',
+            'Marsa Matrouh',
+            'Minya',
+            'Monofiya',
+            'New Valley',
+            'North Sinai',
+            'Port Said',
+            'Qalioubiya',
+            'Qena',
+            'Red Sea',
+            'Sharqiya',
+            'Sohag',
+            'South Sinai',
+            'Suez',
+            'Tanta',
+        ],
+        required: true,
+    },
+    city: {
+        type: String,
+        required: false,
+    },
+    street: {
+        type: String,
+        required: false,
+    },
 });
 
 const paymentMethodSchema = new Schema({
-  bankAccount: [
-    {
-      enable: {
-        //account is valid to use or not (freezed or in reviewing)
-        type: Boolean,
-        default: false,
-      },
-      accNumber: {
-        type: String,
-        // required: true,
-      },
-      iban: {
-        type: String,
-        // required: true,
-      },
-      swiftCode: {
-        type: String,
-        // required: true,
-      },
-      bankDocs: {
-        type: [String], // Define it as an array of strings
-        // required: true, // The entire array is required
-      },
-    },
-  ],
-  fawry: [
-    {
-      enable: {
-        type: Boolean,
-        default: false,
-      },
-      number: {
-        type: String,
-        // required: true,
-      },
+    bankAccount: [
+        {
+            enable: {
+                //account is valid to use or not (freezed or in reviewing)
+                type: Boolean,
+                default: false,
+            },
+            accNumber: {
+                type: String,
+                // required: true,
+            },
+            iban: {
+                type: String,
+                // required: true,
+            },
+            swiftCode: {
+                type: String,
+                // required: true,
+            },
+            bankDocs: {
+                type: [String], // Define it as an array of strings
+                // required: true, // The entire array is required
+            },
+        },
+    ],
+    fawry: [
+        {
+            enable: {
+                type: Boolean,
+                default: false,
+            },
+            number: {
+                type: String,
+                // required: true,
+            },
 
-      fawryDocs: {
-        type: [String],
-        // required: true,
-      },
-    },
-  ],
-  vodafoneCash: [
-    {
-      enable: {
-        type: Boolean,
-        default: false,
-      },
-      number: {
-        type: String,
-        // required: true,
-      },
+            fawryDocs: {
+                type: [String],
+                // required: true,
+            },
+        },
+    ],
+    vodafoneCash: [
+        {
+            enable: {
+                type: Boolean,
+                default: false,
+            },
+            number: {
+                type: String,
+                // required: true,
+            },
 
-      vodafoneCashDocs: {
-        type: [String],
-        // required: true,
-      },
-    },
-  ],
+            vodafoneCashDocs: {
+                type: [String],
+                // required: true,
+            },
+        },
+    ],
 });
 // paymentMethodSchema.path('bankAccount').validate(function (value) {
 //   for (const bankAccount of value) {
@@ -131,167 +136,167 @@ const paymentMethodSchema = new Schema({
 //   return value.length > 0;
 // }, 'At least one vodafoneCash account must be provided.');
 
-const charitySchema = new Schema(
-  {
-    cases: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Case',
-      },
-    ],
-    image: {
-      type: String,
-      required: true, // Ensure it's required
-    }, // profileImg: {},
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    contactInfo: {
-      email: {
-        type: String,
-        required: true,
-      },
-      phone: {
-        type: Number,
-        required: true,
-      },
-      websiteUrl: {
-        type: String,
-        required: true,
-      },
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    totalDonationsIncome: {
-      type: Number,
-      default: 0,
-    },
-    verificationCode: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    emailVerification: {
-      isVerified: {
-        type: Boolean,
-        default: false,
-      },
-      verificationDate: {
-        type: Date,
-        default: null,
-      },
-    },
-    phoneVerification: {
-      isVerified: {
-        type: Boolean,
-        default: false,
-      },
-      verificationDate: {
-        type: Date,
-        default: null,
-      },
-    },
-    isEnabled: {
-      //to freeze account or not
-      type: Boolean,
-      default: true,
-      required: true,
-    },
-    isConfirmed: {
-      //to confirm the docs of the charities
-      type: Boolean,
-      default: false,
-      required: true,
-    },
-    isPending: {
-      //to check if the charity sends its docs
-      type: Boolean,
-      default: false,
-      required: true,
-    },
-    // modifyPaymentMethodsRequest: {
-    //   //if the charity admin request to add a new payment account or edit existing one
-    //   type: Boolean,
-    //   default: false,
-    //   required: true,
-    // },
-    paymentMethods: {
-      type: paymentMethodSchema,
-      // required: true,
-    },
-    rate: {
-      type: Number,
-      default: 0,
-    },
-    donorRequests: [
-      {
-        user: {
-          type: Schema.Types.ObjectId,
-          ref: 'user',
-          required: true,
+const charitySchema: CharitySchema = new Schema(
+    {
+        cases: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Case',
+            },
+        ],
+        image: {
+            type: String,
+            required: true, // Ensure it's required
+        }, // profileImg: {},
+        email: {
+            type: String,
+            required: true,
+            unique: true,
         },
-        requestTitle: {
-          type: String,
-          required: true,
+        password: {
+            type: String,
+            required: true,
         },
-        requestMessage: {
-          type: String,
-          required: true,
+        name: {
+            type: String,
+            required: true,
         },
-      },
-    ],
-    currency: [
-      {
-        type: String,
-        required: true,
-      },
-    ],
-    location: [{ type: locationSchema, required: true }],
-    // files: [{}],
-    charityInfo: {
-      registeredNumber: {
-        type: String,
-        required: true,
-      },
-      establishedDate: {
-        type: String,
-        required: true,
-      },
+        contactInfo: {
+            email: {
+                type: String,
+                required: true,
+            },
+            phone: {
+                type: Number,
+                required: true,
+            },
+            websiteUrl: {
+                type: String,
+                required: true,
+            },
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        totalDonationsIncome: {
+            type: Number,
+            default: 0,
+        },
+        verificationCode: {
+            type: String,
+            required: false,
+            default: null,
+        },
+        emailVerification: {
+            isVerified: {
+                type: Boolean,
+                default: false,
+            },
+            verificationDate: {
+                type: Date,
+                default: null,
+            },
+        },
+        phoneVerification: {
+            isVerified: {
+                type: Boolean,
+                default: false,
+            },
+            verificationDate: {
+                type: Date,
+                default: null,
+            },
+        },
+        isEnabled: {
+            //to freeze account or not
+            type: Boolean,
+            default: true,
+            required: true,
+        },
+        isConfirmed: {
+            //to confirm the docs of the charities
+            type: Boolean,
+            default: false,
+            required: true,
+        },
+        isPending: {
+            //to check if the charity sends its docs
+            type: Boolean,
+            default: false,
+            required: true,
+        },
+        // modifyPaymentMethodsRequest: {
+        //   //if the charity admin request to add a new payment account or edit existing one
+        //   type: Boolean,
+        //   default: false,
+        //   required: true,
+        // },
+        paymentMethods: {
+            type: paymentMethodSchema,
+            // required: true,
+        },
+        rate: {
+            type: Number,
+            default: 0,
+        },
+        donorRequests: [
+            {
+                user: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'user',
+                    required: true,
+                },
+                requestTitle: {
+                    type: String,
+                    required: true,
+                },
+                requestMessage: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        currency: [
+            {
+                type: String,
+                required: true,
+            },
+        ],
+        location: [{ type: locationSchema, required: true }],
+        // files: [{}],
+        charityInfo: {
+            registeredNumber: {
+                type: String,
+                required: true,
+            },
+            establishedDate: {
+                type: String,
+                required: true,
+            },
+        },
+        charityDocs: {
+            docs1: [String],
+            docs2: [String],
+            docs3: [String],
+            docs4: [String],
+        },
+        //, charityReqDocs: {
+        //   docs: [String],
+        // },
     },
-    charityDocs: {
-      docs1: [String],
-      docs2: [String],
-      docs3: [String],
-      docs4: [String],
-    },
-    //, charityReqDocs: {
-    //   docs: [String],
-    // },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 charitySchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    //to not change password every time we edit the user data
-    next();
-  }
-  const salt = await bcrypt.genSalt(
-    configurationProvider.getValue('hashing.salt')
-  );
-  this.password = await bcrypt.hash(this.password, salt);
+    if (!this.isModified('password')) {
+        //to not change password every time we edit the user data
+        next();
+    }
+    const salt = await bcrypt.genSalt(
+        configurationProvider.getValue('hashing.salt')
+    );
+    this.password = await bcrypt.hash(this.password, salt);
 });
 // const editImgUrl = (doc) => {
 //   if (doc.image) {
@@ -473,6 +478,9 @@ charitySchema.pre('save', async function (next) {
 //     this.getUpdate().$set.password = await bcrypt.hash(passwordToUpdate, salt);
 //   }
 // });
-const Charity = mongoose.model('Charity', charitySchema);
+const Charity: CharityModel = mongoose.model<CharityDocument, CharityModel>(
+    'Charity',
+    charitySchema
+);
 
 export default Charity;
