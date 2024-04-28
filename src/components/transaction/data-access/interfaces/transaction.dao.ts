62f0b53fdf0bdd337e3b99b59366261bbb1f0946
SaifLotfi <swaiflnkac@gmail.com>
2024-04-28 19:17:07 +0300

    fixing transaction dao

--- src/components/transaction/data-access/interfaces/transaction.dao.ts
+++ src/components/transaction/data-access/interfaces/transaction.dao.ts
@@ -1,13 +1,8 @@
 import { ICase } from '../../../case/data-access/interfaces/case.interface';
 import { ICharity } from '../../../charity/data-access/interfaces/charity.interface';
-<<<<<<< HEAD
 import { User } from '../../../user/data-access/interfaces';
-import { ITransaction } from '../models/transaction.model';
-=======
-import { User } from '../../../user/data-access/models/user.model';
 import { ITransaction } from '.';
 
->>>>>>> 5ba5c350bf43747b0a6ddf7e937ec3225f4e8e9e
 export interface TransactionDataStore {
   findCaseById(id: string): Promise<ICase | null>;
   findCharityById(id: string): Promise<ICharity | null>;
