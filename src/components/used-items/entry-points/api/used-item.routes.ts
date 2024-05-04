a92d07294f6b095a95cb39494ef5135da3cd7aea
SaifLotfi <swaiflnkac@gmail.com>
2024-05-04 09:11:59 +0300

    added isUser utility #193

--- src/components/used-items/entry-points/api/used-item.routes.ts
+++ src/components/used-items/entry-points/api/used-item.routes.ts
@@ -1,7 +1,7 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import logger from '../../../../utils/logger';
 import { usedItemUseCase } from '../../domain/used-item.use-case';
-import { auth, isActivated } from '../../../auth/shared';
+import { auth, isActivated, isUser } from '../../../auth/shared';
 export default function defineRoutes(expressApp: Application) {
   const router = express.Router();
 
@@ -9,10 +9,15 @@ export default function defineRoutes(expressApp: Application) {
     '/',
     auth,
     isActivated,
+    isUser,
     async (req: Request, res: Response, next: NextFunction) => {
       try {
         logger.info(`Used Items API was called to Create Used Item`);
-        const createUsedItemResponse= await usedItemUseCase.addUsedItem(req, res, next);
+        const createUsedItemResponse = await usedItemUseCase.addUsedItem(
+          req,
+          res,
+          next
+        );
         return res.json(createUsedItemResponse);
       } catch (error) {
         next(error);
@@ -21,52 +26,51 @@ export default function defineRoutes(expressApp: Application) {
     }
   );
 
-  router.route(
-    '/:id')
-    .delete(
-    auth,
-    isActivated,
-    async (req: Request, res: Response, next: NextFunction) => {
+  router
+    .route('/:id')
+    .all(auth, isActivated, isUser)
+    .delete(async (req: Request, res: Response, next: NextFunction) => {
       try {
         logger.info(`Used Items API was called to Delete Used Item`);
-        const logOutResponse = await usedItemUseCase.deleteUsedItem(req, res, next);
+        const logOutResponse = await usedItemUseCase.deleteUsedItem(
+          req,
+          res,
+          next
+        );
         return res.json(logOutResponse);
       } catch (error) {
         next(error);
         return undefined;
       }
-    }
-  )
-  .put(
-    auth,
-    isActivated,
-    async (req: Request, res: Response, next: NextFunction) => {
+    })
+    .put(async (req: Request, res: Response, next: NextFunction) => {
       try {
         logger.info(`Used Items API was called to Update Used Item`);
-        const updateUsedItemResponse = await usedItemUseCase.updateUsedItem(req, res, next);
+        const updateUsedItemResponse = await usedItemUseCase.updateUsedItem(
+          req,
+          res,
+          next
+        );
         return res.json(updateUsedItemResponse);
       } catch (error) {
         next(error);
         return undefined;
       }
-    }
-  )
-  .get(
-    auth,
-    isActivated,
-    async (req: Request, res: Response, next: NextFunction) => {
+    })
+    .get(async (req: Request, res: Response, next: NextFunction) => {
       try {
         logger.info(`Used Items API was called to Get Used Item`);
-        const getUsedItemResponse = await usedItemUseCase.getUsedItem(req, res, next);
+        const getUsedItemResponse = await usedItemUseCase.getUsedItem(
+          req,
+          res,
+          next
+        );
         return res.json(getUsedItemResponse);
       } catch (error) {
         next(error);
         return undefined;
       }
-    }
-  );
-  
-  
+    });
 
   expressApp.use('/api/usedItem', router);
 }
