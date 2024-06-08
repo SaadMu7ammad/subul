1d2e5a5a42401ad1e00d78313512bd9a5c68e3bf
SaifLotfi <swaiflnkac@gmail.com>
2024-06-08 12:58:51 +0300

    configuring DB & generateTestingToken & createDummyUser #210

--- src/components/used-items/test/add-usedItem.test.ts
+++ src/components/used-items/test/add-usedItem.test.ts
@@ -1,14 +1,17 @@
 import { beforeAll, describe } from '@jest/globals';
 import axios from 'axios';
 
+import { createDummyUserAndReturnToken } from './test-helpers';
+
 let axiosAPIClient;
 
 beforeAll(async () => {
+  const token = await createDummyUserAndReturnToken();
   const axiosConfig = {
     baseURL: `http://localhost:${0}`,
     validateStatus: () => true, // Don't throw HTTP exceptions. Delegate to the tests to decide which error is acceptable
     headers: {
-      // cookie:`bearer=${}`
+      cookie: `jwt=${token}`,
     },
   };
   axiosAPIClient = axios.create(axiosConfig);
--- src/components/used-items/test/test-helpers.ts
+++ src/components/used-items/test/test-helpers.ts
@@ -1,3 +1,10 @@
+import { PlainUser } from '@components/user/data-access/interfaces';
+import { userRepository as UserRepository } from '@components/user/data-access/user.repository';
+import connectDB from '@config/db';
+import { generateTokenForTesting } from '@utils/generateToken';
+
+(async () => await connectDB())();
+
 /*
 1. make a test DB
     put the url in the config file and use the .env to determine which Url to be used (test | dev | prod)
@@ -8,4 +15,37 @@
 2. create a dummy verified user
 3. return the token of the dummy user
 */
-const createDummyUser = () => {};
+export const createDummyUserAndReturnToken = async () => {
+  const userRepository = new UserRepository();
+
+  const dummyUserData: PlainUser = {
+    name: {
+      firstName: 'dummy',
+      lastName: 'dummy',
+    },
+    email: 'dummy@dummy.com',
+    password: 'dummy',
+    phone: '01234567890',
+    gender: 'male',
+    userLocation: {
+      governorate: 'Cairo',
+    },
+    isAdmin: false,
+    contributions: [],
+    isEnabled: true,
+    pointsOnDonations: 0,
+    totalDonationsAmount: 0,
+    transactions: [],
+    verificationCode: 'dummy',
+    emailVerification: {
+      isVerified: true,
+      verificationDate: new Date().toString(),
+    },
+  };
+
+  const dummyUser = await userRepository.createUser(dummyUserData);
+
+  const token = generateTokenForTesting(dummyUser._id.toString(), 'user');
+
+  return token;
+};
