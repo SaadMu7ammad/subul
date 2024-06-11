24ee575874c78080fa17d19e24045ef0c24a6aa5
SaifLotfi <swaiflnkac@gmail.com>
2024-06-11 12:54:45 +0300

    adding the first successful integration test & its configuration & helpers #210

--- src/components/used-items/test/add-usedItem.test.ts
+++ src/components/used-items/test/add-usedItem.test.ts
@@ -1,22 +1,62 @@
-import { beforeAll, describe } from '@jest/globals';
-import axios from 'axios';
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import { startWebServer, stopWebServer } from '@utils/server';
+import axios, { AxiosInstance } from 'axios';
+import mongoose from 'mongoose';
+import nock from 'nock';
 
-import { createDummyUserAndReturnToken } from './test-helpers';
+import {
+  clearUsedItemsDatabase,
+  clearUserDatabase,
+  createDummyUserAndReturnToken,
+} from './test-helpers';
 
-let axiosAPIClient;
-
+let axiosAPIClient: AxiosInstance;
+/*
+we need to spin up the server before running the tests. 
+*/
 beforeAll(async () => {
+  const apiConnection = await startWebServer();
   const token = await createDummyUserAndReturnToken();
+
   const axiosConfig = {
-    baseURL: `http://localhost:${0}`,
+    baseURL: `http://127.0.0.1:${apiConnection.port}`,
     validateStatus: () => true, // Don't throw HTTP exceptions. Delegate to the tests to decide which error is acceptable
     headers: {
       cookie: `jwt=${token}`,
     },
   };
   axiosAPIClient = axios.create(axiosConfig);
+  nock.disableNetConnect();
+  nock.enableNetConnect('127.0.0.1');
+});
+
+afterAll(async () => {
+  await clearUserDatabase();
+  await clearUsedItemsDatabase();
+  nock.enableNetConnect();
+  mongoose.connection.close();
+  stopWebServer();
 });
 
 describe('/api/usedItem', () => {
-  describe('POST /', () => {});
+  describe('POST /', () => {
+    test('When adding a new valid usedItem, Then should get back approval with 200 response', async () => {
+      //Arrange
+      const usedItem = {
+        title: 'Used Item 1',
+        category: 'clothes',
+        description: 'This is a used item',
+        images: ['image1.png', 'image2.png'],
+        amount: 10,
+      };
+
+      //Act
+      const { data, status } = await axiosAPIClient.post('/api/usedItem', usedItem);
+
+      //Assert
+      expect(status).toBe(200);
+      expect(typeof data.usedItem._id).toBe('string');
+      expect(data.usedItem._id).toBeDefined();
+    });
+  });
 });
--- src/components/used-items/test/test-helpers.ts
+++ src/components/used-items/test/test-helpers.ts
@@ -1,20 +1,10 @@
 import { PlainUser } from '@components/user/data-access/interfaces';
+import UserModel from '@components/user/data-access/models/user.model';
 import { userRepository as UserRepository } from '@components/user/data-access/user.repository';
-import connectDB from '@config/db';
 import { generateTokenForTesting } from '@utils/generateToken';
 
-(async () => await connectDB())();
+import UsedItem from '../data-access/models/used-item.model';
 
-/*
-1. make a test DB
-    put the url in the config file and use the .env to determine which Url to be used (test | dev | prod)
-    >> OFC jest sets the .env environment to 'test'
-    OR
-    create it here ?
-    >> We should use the a local mongo db , Edit : for simplicity we can use a cloud DB
-2. create a dummy verified user
-3. return the token of the dummy user
-*/
 export const createDummyUserAndReturnToken = async () => {
   const userRepository = new UserRepository();
 
@@ -48,4 +38,12 @@ export const createDummyUserAndReturnToken = async () => {
   const token = generateTokenForTesting(dummyUser._id.toString(), 'user');
 
   return token;
+};
+
+export const clearUserDatabase = async () => {
+  await UserModel.deleteMany({});
+};
+
+export const clearUsedItemsDatabase = async () => {
+  await UsedItem.deleteMany({});
 };
