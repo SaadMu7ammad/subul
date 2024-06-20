d6a1f5b51b7fec35462589c79507c921056e2fbd
SaifLotfi <swaiflnkac@gmail.com>
2024-06-20 15:00:14 +0300

    added test: register-user | unhappy scenarios #215

--- src/components/auth/user/test/register-user.test.ts
+++ src/components/auth/user/test/register-user.test.ts
@@ -44,5 +44,42 @@ describe('/api/users', () => {
       expect(response.data.user.email).toBe(user.email);
       expect(response.data.user.name.firstName).toBe(user.name.firstName);
     });
+
+    test('should return 400 when trying to register a user with an email that is already registered', async () => {
+      // Arrange
+      const user = getDummyUserObject();
+      await axiosAPIClient.post('/api/users', user);
+
+      // Act
+      const response = await axiosAPIClient.post('/api/users', user);
+
+      // Assert
+      expect(response.status).toBe(400);
+    });
+
+    test('should return 400 when trying to register a user with invalid values', async () => {
+      // Arrange
+      const user = getDummyUserObject();
+      user.email = 'invalid-email';
+
+      // Act
+      const response = await axiosAPIClient.post('/api/users', user);
+
+      // Assert
+      expect(response.status).toBe(400);
+    });
+
+    test('should return 500 when trying to register a user with missing values', async () => {
+      // Arrange
+      const user = getDummyUserObject();
+      //@ts-expect-error // We are testing the API, so we can delete the email field
+      delete user.email;
+
+      // Act
+      const response = await axiosAPIClient.post('/api/users', user);
+
+      // Assert
+      expect(response.status).toBe(500);
+    });
   });
 });
