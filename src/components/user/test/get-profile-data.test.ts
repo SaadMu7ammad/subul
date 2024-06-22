31df370524af0c44a60735187e3774106b2038c5
SaifLotfi <swaiflnkac@gmail.com>
2024-06-22 14:35:19 +0300

    added test: get-profile-data #215

---
+++ src/components/user/test/get-profile-data.test.ts
@@ -0,0 +1,47 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, test } from '@jest/globals';
+import { startWebServer, stopWebServer } from '@src/server';
+import axios, { AxiosInstance } from 'axios';
+import mongoose from 'mongoose';
+import nock from 'nock';
+
+import { clearUserDatabase, createDummyUserAndReturnToken } from './test-helpers';
+
+let axiosAPIClient: AxiosInstance;
+
+beforeAll(async () => {
+  const apiConnection = await startWebServer();
+  const token = await createDummyUserAndReturnToken();
+
+  const axiosConfig = {
+    baseURL: `http://127.0.0.1:${apiConnection.port}`,
+    validateStatus: () => true, // Don't throw HTTP exceptions. Delegate to the tests to decide which error is acceptable
+    headers: {
+      cookie: `jwt=${token}`,
+    },
+  };
+  axiosAPIClient = axios.create(axiosConfig);
+  nock.disableNetConnect();
+  nock.enableNetConnect('127.0.0.1');
+});
+
+beforeEach(async () => {});
+
+afterAll(async () => {
+  await clearUserDatabase();
+  nock.enableNetConnect();
+  mongoose.connection.close();
+  stopWebServer();
+});
+
+describe('/api/users', () => {
+  describe('GET /profile', () => {
+    test('Should return profile data with 200 status code', async () => {
+      //Act
+      const response = await axiosAPIClient.get('/api/users/profile');
+
+      //Assert
+      expect(response.status).toBe(200);
+      expect(response.data.user.email).toBe('dummy@dummy.com');
+    });
+  });
+});
