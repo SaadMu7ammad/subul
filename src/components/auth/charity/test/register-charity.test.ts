a10a19bd557b572b347024b1cf60d6e74bed8073
SaifLotfi <swaiflnkac@gmail.com>
2024-06-22 12:12:12 +0300

    added test: register-charity #216

---
+++ src/components/auth/charity/test/register-charity.test.ts
@@ -0,0 +1,90 @@
+import Charity from '@components/charity/data-access/models/charity.model';
+import { afterAll, beforeAll, beforeEach, describe, expect, test } from '@jest/globals';
+import { startWebServer, stopWebServer } from '@src/server';
+import axios, { AxiosInstance } from 'axios';
+import FormData from 'form-data';
+import mongoose from 'mongoose';
+import nock from 'nock';
+
+import {
+  appendDummyCharityToFormData,
+  appendDummyImageToFormData,
+  clearCharityDatabase,
+  getDummyCharityObject,
+} from './test-helpers';
+
+let axiosAPIClient: AxiosInstance;
+
+beforeAll(async () => {
+  const apiConnection = await startWebServer();
+
+  const axiosConfig = {
+    baseURL: `http://127.0.0.1:${apiConnection.port}`,
+    validateStatus: () => true, // Don't throw HTTP exceptions. Delegate to the tests to decide which error is acceptable
+  };
+  axiosAPIClient = axios.create(axiosConfig);
+  nock.disableNetConnect();
+  nock.enableNetConnect('127.0.0.1');
+});
+
+beforeEach(async () => {
+  await clearCharityDatabase();
+});
+
+afterAll(async () => {
+  await clearCharityDatabase();
+  nock.enableNetConnect();
+  mongoose.connection.close();
+  stopWebServer();
+});
+
+describe('/api/charities', () => {
+  describe('POST /register', () => {
+    test('should register a new charity with success', async () => {
+      //Arrange
+      const formData = new FormData();
+      appendDummyImageToFormData(formData);
+      appendDummyCharityToFormData(formData);
+
+      //Act
+      const response = await axiosAPIClient.post('/api/charities/register', formData, {
+        headers: formData.getHeaders(),
+      });
+
+      //Assert
+      expect(response.status).toBe(200);
+    });
+  });
+
+  test('should not register a new charity with missing data', async () => {
+    //Arrange
+    const formData = new FormData();
+    formData.append('name', 'Charity Name');
+
+    //Act
+    const response = await axiosAPIClient.post('/api/charities/register', formData, {
+      headers: formData.getHeaders(),
+    });
+
+    //Assert
+    expect(response.status).toBe(400);
+  });
+
+  test('should not register a charity with email already in use', async () => {
+    //Arrange
+    const formData = new FormData();
+    appendDummyImageToFormData(formData);
+    appendDummyCharityToFormData(formData);
+
+    //Act
+    //Store a charity with the same email
+    await Charity.create(getDummyCharityObject());
+
+    const response = await axiosAPIClient.post('/api/charities/register', formData, {
+      headers: formData.getHeaders(),
+    });
+
+    //Assert
+    expect(response.status).toBe(400);
+  });
+});
