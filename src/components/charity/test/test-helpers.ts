f6643ba704141c8921d3fc288c40dfe61bc7f64a
SaifLotfi <swaiflnkac@gmail.com>
2024-06-23 11:42:14 +0300

    added test: show-charity-profile #222

---
+++ src/components/charity/test/test-helpers.ts
@@ -0,0 +1,79 @@
+import { CharityRepository } from '@components/charity/data-access/charity.repository';
+import { PlainCharity } from '@components/charity/data-access/interfaces';
+import { Charity as CharityModel } from '@components/charity/data-access/models/charity.model';
+import { generateTokenForTesting } from '@utils/generateToken';
+import axios from 'axios';
+import FormData from 'form-data';
+import fs from 'fs';
+import path from 'path';
+
+export const createDummyCharityAndReturnToken = async () => {
+  const charityRepository = new CharityRepository();
+
+  const dummyCharityData: PlainCharity = {
+    cases: [],
+    image: 'image.png',
+    email: 'dummy@dummy.ape',
+    password: 'dummy',
+    name: 'dummy',
+    contactInfo: {
+      email: 'dummy@dummy.ape',
+      phone: '1234567890',
+      websiteUrl: 'dummy.ape',
+    },
+    description: 'dummy',
+    emailVerification: {
+      isVerified: true,
+      verificationDate: new Date().toString(),
+    },
+    isEnabled: true,
+    isConfirmed: true,
+    isPending: false,
+    currency: ['EGP'],
+    charityLocation: [
+      //@ts-expect-error don't care much when we are testing :P
+      {
+        governorate: 'Cairo',
+      },
+    ],
+    charityDocs: {
+      docs1: ['doc1.png'],
+      docs2: ['doc2.png'],
+      docs3: ['doc3.png'],
+      docs4: ['doc4.png'],
+    },
+    charityInfo: {
+      establishedDate: new Date().toString(),
+      registeredNumber: '1234567890',
+    },
+  };
+
+  const dummyCharity = await charityRepository.createCharity(dummyCharityData);
+
+  const token = generateTokenForTesting(dummyCharity!._id.toString(), 'charity');
+
+  return token;
+};
+
+export const clearCharityDatabase = async () => {
+  await CharityModel.deleteMany({});
+};
+
+export const appendDummyImagesToFormData = (formData: FormData) => {
+  const imagePath = path.resolve(__dirname, 'test-image.png');
+  const imageBuffer = fs.readFileSync(imagePath);
+  for (let i = 1; i <= 5; i++) {
+    formData.append('images', imageBuffer, `test-image${i}.png`);
+  }
+};
+
+export const createAxiosApiClient = (token: string, port: number) => {
+  const axiosConfig = {
+    baseURL: `http://127.0.0.1:${port}`,
+    validateStatus: () => true, // Don't throw HTTP exceptions. Delegate to the tests to decide which error is acceptable
+    headers: {
+      cookie: `jwt=${token}`,
+    },
+  };
+  return axios.create(axiosConfig);
+};
