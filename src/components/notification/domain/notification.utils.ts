6263bc5f9294c60ca086b268f529de0205fa9e77
SaadMu7ammad <saadmu7ammad711711@gmail.com>
2024-06-30 18:45:17 +0300

    add classes to `notification`

--- src/components/notification/domain/notification.utils.ts
+++ src/components/notification/domain/notification.utils.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 import { BadRequestError } from '../../../libraries/errors/components/bad-request';
 import { NotFoundError } from '../../../libraries/errors/components/not-found';
+import { notificationUtilsSkeleton } from '../data-access/interfaces';
 import {
   FilterObj,
   GetAllNotificationsQueryParams,
@@ -12,125 +13,134 @@ import {
 } from '../data-access/interfaces/notification.interface';
 import { NotificationRepository } from '../data-access/notification.repository';
 
-const notificationRepository = new NotificationRepository();
+export class notificationUtilsClass implements notificationUtilsSkeleton {
+  notificationRepository: NotificationRepository;
 
-const getAllNotifications = async (
-  filterObj: FilterObj,
-  sortObj: SortObj,
-  paginationObj: PaginationObj
-) => {
-  const notifications = await notificationRepository.getAllNotifications(
-    filterObj,
-    sortObj,
-    paginationObj
-  );
-  return notifications;
-};
+  constructor() {
+    this.notificationRepository = new NotificationRepository();
+  }
 
-const markNotificationAsRead = async (notification: INotification) => {
-  notification.read = true;
+  async getAllNotifications(
+    filterObj: FilterObj,
+    sortObj: SortObj,
+    paginationObj: PaginationObj
+  ): Promise<INotification[]> {
+    const notifications = await this.notificationRepository.getAllNotifications(
+      filterObj,
+      sortObj,
+      paginationObj
+    );
+    return notifications;
+  }
 
-  await notification.save();
+  async markNotificationAsRead(notification: INotification): Promise<INotification> {
+    notification.read = true;
 
-  return notification;
-};
+    await notification.save();
 
-const deleteNotification = async (
-  receiverType: string,
-  receiverId: string,
-  notification: INotification
-) => {
-  const deletedNotification = await notificationRepository.deleteNotificationById(
-    receiverType,
-    receiverId,
-    notification._id.toString()
-  );
+    return notification;
+  }
 
-  if (!deletedNotification) throw new NotFoundError('Notification Not Found');
-
-  return notification;
-};
-
-const getSortObj = (sortQueryParams: string | undefined) => {
-  const sortBy = sortQueryParams || 'createdAt';
-
-  const sortArray = sortBy.split(',');
-
-  const sortObj: SortObj = {};
-  sortArray.forEach(function (sort) {
-    if (sort[0] === '-') {
-      sortObj[sort.substring(1)] = -1;
-    } else {
-      sortObj[`${sort}`] = 1;
-    }
-  });
-
-  return sortObj;
-};
-
-const getPaginationObj = (queryParams: GetAllNotificationsQueryParams) => {
-  const limit = queryParams?.limit ? +queryParams.limit : 10;
-
-  const page = queryParams?.page ? +queryParams.page : 1;
-
-  const offset = queryParams?.offset ? +queryParams.offset : 0;
-
-  return { limit, page, offset };
-};
-
-const getFilterObj = (
-  receiverType: string,
-  receiverId: string,
-  queryParams: GetAllNotificationsQueryParams
-) => {
-  const filterObject: FilterObj = {
-    receiver: {
+  async deleteNotification(
+    receiverType: string,
+    receiverId: string,
+    notification: INotification
+  ): Promise<INotification> {
+    const deletedNotification = await this.notificationRepository.deleteNotificationById(
       receiverType,
-      receiverId: new mongoose.Types.ObjectId(receiverId),
-    },
-  };
+      receiverId,
+      notification._id.toString()
+    );
 
-  if (queryParams.read) {
-    filterObject['read'] = queryParams.read === 'true';
+    if (!deletedNotification) throw new NotFoundError('Notification Not Found');
+
+    return notification;
   }
 
-  return filterObject;
-};
+  getSortObj(sortQueryParams: string | undefined): SortObj {
+    const sortBy = sortQueryParams || 'createdAt';
 
-const getNotification = async (
-  receiverType: string,
-  receiverId: string,
-  notificationId: string
-) => {
-  const notification = await notificationRepository.getNotificationById(
-    receiverType,
-    receiverId,
-    notificationId
-  );
+    const sortArray = sortBy.split(',');
 
-  if (!notification) throw new NotFoundError('Notification Not Found');
+    const sortObj: SortObj = {};
+    sortArray.forEach(function (sort) {
+      if (sort[0] === '-') {
+        sortObj[sort.substring(1)] = -1;
+      } else {
+        sortObj[`${sort}`] = 1;
+      }
+    });
 
-  return notification;
-};
-
-const validateIdParam = (id: string | undefined): asserts id is string => {
-  if (!id) {
-    throw new BadRequestError('No id provided');
+    return sortObj;
   }
-};
 
-const deleteOutdatedNotifications = async (receiverType: ReceiverType, receiverId: string) => {
-  await notificationRepository.deleteOutdatedNotifications(receiverType, receiverId);
-};
+  getPaginationObj(queryParams: GetAllNotificationsQueryParams): {
+    limit: number;
+    page: number;
+    offset: number;
+  } {
+    const limit = queryParams?.limit ? +queryParams.limit : 10;
 
-export const notificationUtils = {
-  getAllNotifications,
-  markNotificationAsRead,
-  deleteNotification,
-  getSortObj,
-  getPaginationObj,
-  getFilterObj,
-  getNotification,
-  validateIdParam,
-  deleteOutdatedNotifications,
-};
+    const page = queryParams?.page ? +queryParams.page : 1;
+
+    const offset = queryParams?.offset ? +queryParams.offset : 0;
+
+    return { limit, page, offset };
+  }
+
+  getFilterObj(
+    receiverType: string,
+    receiverId: string,
+    queryParams: GetAllNotificationsQueryParams
+  ): FilterObj {
+    const filterObject: FilterObj = {
+      receiver: {
+        receiverType,
+        receiverId: new mongoose.Types.ObjectId(receiverId),
+      },
+    };
+
+    if (queryParams.read) {
+      filterObject['read'] = queryParams.read === 'true';
+    }
+
+    return filterObject;
+  }
+
+  async getNotification(
+    receiverType: string,
+    receiverId: string,
+    notificationId: string
+  ): Promise<INotification> {
+    const notification = await this.notificationRepository.getNotificationById(
+      receiverType,
+      receiverId,
+      notificationId
+    );
+
+    if (!notification) throw new NotFoundError('Notification Not Found');
+
+    return notification;
+  }
+
+  validateIdParam(id: string | undefined): asserts id is string {
+    if (!id) {
+      throw new BadRequestError('No id provided');
+    }
+  }
+
+  async deleteOutdatedNotifications(receiverType: ReceiverType, receiverId: string): Promise<void> {
+    await this.notificationRepository.deleteOutdatedNotifications(receiverType, receiverId);
+  }
+}
+// export const notificationUtils = {
+//   getAllNotifications,
+//   markNotificationAsRead,
+//   deleteNotification,
+//   getSortObj,
+//   getPaginationObj,
+//   getFilterObj,
+//   getNotification,
+//   validateIdParam,
+//   deleteOutdatedNotifications,
+// };
